686fc621e12cb4b8cad35990f9d12681b8d12ee6
yapyeeqiang <yapyeeqiang@gmail.com>
2023-05-10 15:15:13 +0800

    add: pcsc integration

--- app.js
+++ app.js
@@ -1,50 +1,64 @@
-import pcsclite from 'pcsclite'
-import fs from 'fs'
-import { exec } from 'child_process'
-import { outputFile, outputUserFile, patterns, runFile } from './const.js'
+import pcsclite from "pcsclite";
+import fs from "fs";
+import { execSync } from "child_process";
+import { outputFile, outputUserFile, patterns, runFile } from "./const.js";
 
-const pcsc = pcsclite()
+const pcsc = pcsclite();
 
-pcsc.on('reader', (reader) => {
-    console.log(`Reader '${reader.name}' detected`)
+pcsc.on("reader", (reader) => {
+  console.log(`Reader '${reader.name}' detected`);
 
-    reader.on('status', (status) => {
-        const changes = reader.state ^ status.state
+  reader.on("status", (status) => {
+    const changes = reader.state ^ status.state;
 
-        if (changes & reader.SCARD_STATE_PRESENT && status.state & reader.SCARD_STATE_PRESENT) {
-            console.log('Card inserted')
+    if (
+      changes & reader.SCARD_STATE_PRESENT &&
+      status.state & reader.SCARD_STATE_PRESENT
+    ) {
+      console.log("Card inserted");
 
-            scanIC()
-        }
-    })
-})
+      scanIC();
 
-function extractInformation() {
-    const data = fs.readFileSync(outputFile, 'utf8')
+      console.log("Done");
+    }
 
-    const result = patterns.reduce((acc, { key, pattern }) => {
-        const match = data.match(pattern)
-
-        if (match) {
-            acc[key] = match[1]
-                .trim()
-                .replace(/[\r\n]+/g, ', ')
-                .replace(/\t/g, ' ')
-        }
-
-        return acc
-    }, {})
-
-    fs.writeFileSync(outputUserFile, JSON.stringify(result))
-}
+    if (
+      changes & reader.SCARD_STATE_EMPTY &&
+      status.state & reader.SCARD_STATE_EMPTY
+    ) {
+      console.log("Card removed");
+    }
+  });
+});
 
 function scanIC() {
-    exec(runFile, (error) => {
-        if (error) {
-            console.log(error)
-            return
-        }
+  const response = execSync(runFile, (error) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+  });
 
-        extractInformation()
-    })
+  if (response) {
+    extractInformation();
+  }
+}
+
+function extractInformation() {
+  const data = fs.readFileSync(outputFile, "utf8");
+
+  const result = patterns.reduce((acc, { key, pattern }) => {
+    const match = data.match(pattern);
+
+    if (match) {
+      acc[key] = match[1]
+        .trim()
+        .replace(/[\r\n]+/g, ", ")
+        .replace(/\t/g, " ");
+    }
+
+    return acc;
+  }, {});
+
+  fs.writeFileSync(outputUserFile, JSON.stringify(result));
 }
